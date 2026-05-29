a7d96a10f2dc89c21084a63ac7dc6b93287ee8e5
sabaiyeal <yyealpanchaya@gmail.com>
2026-05-29 13:16:14 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4tjjRiQ2bzxKZBQsztItIugdNH5UPIjFM",
+  apiKey: "AIzaSyB4tjjRIq2bzxKZBQszItIugdNH5UPIjFM",
   authDomain: "toobtubb-714e7.firebaseapp.com",
   projectId: "toobtubb-714e7",
   storageBucket: "toobtubb-714e7.firebasestorage.app",
   messagingSenderId: "402491784083",
   appId: "1:402491784083:web:896a7ec84f1f2c8a958666",
-  measurementId: "G-SCXDGBG7W"
+  measurementId: "G-SCXDGBBG7W"
 };
 
+// เริ่มต้นเปิดการทำงานของ Firebase
 const app = initializeApp(firebaseConfig);
+
+// ส่งออกฐานข้อมูล Firestore เพื่อให้ App.tsx ดึงไปใช้บันทึกข้อมูลลูกดิ้น
 export const db = getFirestore(app);

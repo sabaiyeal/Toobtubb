8abde1d42f1250a3222165e9b6507d155686ee60
sabaiyeal <yyealpanchaya@gmail.com>
2026-06-09 16:38:18 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import liff from "@line/liff";
 
-createRoot(document.getElementById("root")!).render(<App />);
+liff.init({ liffId: import.meta.env.VITE_LIFF_ID }).finally(() => {
+  createRoot(document.getElementById("root")!).render(<App />);
+});
